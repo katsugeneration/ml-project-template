02636acdaec3947c54dab4c5a8e86ffacabf9ac6
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-08-16 00:48:30 +0900

    Add step learning rate decay mode

--- model/base.py
+++ model/base.py
@@ -59,6 +59,7 @@ class KerasImageClassifierBase(KerasModelBase):
         lr (float): initial learning rate.
         momentum (float): momentum value.
         clipnorm (float): clipnorm value
+        lr_step_decay (bool): whether to use step learning rate decay.
 
     """
 
@@ -70,6 +71,7 @@ class KerasImageClassifierBase(KerasModelBase):
             lr: float = 0.1,
             momentum: float = 0.9,
             clipnorm: float = 1.0,
+            lr_step_decay: bool = True,
             **kwargs: Any) -> None:
         """Intialize parameter and build model."""
         super(KerasImageClassifierBase, self).__init__(**kwargs)
@@ -79,6 +81,7 @@ class KerasImageClassifierBase(KerasModelBase):
         self.lr = lr
         self.momentum = momentum
         self.clipnorm = clipnorm
+        self.lr_step_decay = lr_step_decay
 
     def setup(self) -> None:
         """Set optimizer to model."""
@@ -99,13 +102,19 @@ class KerasImageClassifierBase(KerasModelBase):
             log (Dict[str, List[Any]]): training log.
 
         """
+        callbacks: List = []
+        if self.lr_step_decay:
+            callbacks.append(tf.keras.callbacks.LearningRateScheduler(
+                    lambda e: self.lr if e < int(self.epochs / 2) else self.lr / 10.0 if e < int(self.epochs * 3 / 4) else self.lr / 100.0))
+
         generator = self.dataset.training_data_generator()
         (x_test, y_test) = self.dataset.eval_data()
         history = self.fit_generator(
                         generator,
                         steps_per_epoch=self.dataset.steps_per_epoch,
                         validation_data=(x_test, y_test),
-                        epochs=self.epochs)
+                        epochs=self.epochs,
+                        callbacks=callbacks)
         return history.history
 
     def inference(self) -> Tuple[List[List[float]], List[Any]]:
