aa597dfe6e47647a9ceb2bda00f10c62a49e923f
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-08-16 00:23:29 +0900

    Add standardization preprocessing to cifar100 dataset

--- dataset/cifar100.py
+++ dataset/cifar100.py
@@ -1,5 +1,6 @@
 from typing import Any
 import tensorflow as tf
+import numpy as np
 from dataset.base import BinaryImageClassifierDataset
 
 
@@ -7,7 +8,7 @@ class Cifar100Dataset(BinaryImageClassifierDataset):
     """Cifar10 dataset loader.
 
     Args:
-        data_normalize_style (str): data noramlization style. Allowed valus are 0to1 ot -1to1
+        data_normalize_style (str): data noramlization style. Allowed valus are 0to1 or -1to1 or standardization
 
     """
 
@@ -16,7 +17,7 @@ class Cifar100Dataset(BinaryImageClassifierDataset):
 
     def __init__(
             self,
-            data_normalize_style: str = '0to1',
+            data_normalize_style: str = 'standardization',
             **kwargs: Any) -> None:
         """Load data and setup preprocessing."""
         super(Cifar100Dataset, self).__init__(**kwargs)
@@ -27,6 +28,11 @@ class Cifar100Dataset(BinaryImageClassifierDataset):
             x_train, x_test = x_train / 255.0, x_test / 255.0
         elif data_normalize_style == '-1to1':
             x_train, x_test = (x_train - 127.5) / 127.5, (x_test - 127.5) / 127.5
+        elif data_normalize_style == 'standardization':
+            mean = np.mean(x_train, axis=(0, 1, 2))
+            std = np.std(x_train, axis=(0, 1, 2))
+            x_train = (x_train - mean) / std
+            x_test = (x_test - mean) / std
         else:
             raise ValueError('Data normaliztion style: {} is not supported.'.format(data_normalize_style))
 
