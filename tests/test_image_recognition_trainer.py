55241fc94ed63cb33bb255d180ba81f4c55778c9
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-08-19 00:42:43 +0900

    Add task runner test

---
+++ tests/test_image_recognition_trainer.py
@@ -0,0 +1,20 @@
+# Copyright 2019 Katsuya Shimabukuro. All rights reserved.
+# Licensed under the MIT License.
+import pathlib
+from nose.tools import ok_, eq_
+from runner.image_recognition_trainer import ImageRecognitionTrainer
+
+
+class TestImageRecognitionTrainer(object):
+    def test_init(self):
+        runner = ImageRecognitionTrainer()
+
+    def test_run(self):
+        path = pathlib.Path('test_log')
+        if not path.exists():
+            path.mkdir(parents=True)
+        runner = ImageRecognitionTrainer()
+        history = runner.run('fcnn', 'mnist', {'epochs': 1}, {'batch_size': 128}, path)
+
+        ok_(path.joinpath('model.h5').exists())
+        ok_(path.joinpath('confusion_matrix.png').exists())
