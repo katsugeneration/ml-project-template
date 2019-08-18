75d93a74a00fd040890f694bfbd4b4fc2ef66850
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2019-08-18 21:56:01 +0900

    Add runner class and task file

---
+++ runner/base.py
@@ -0,0 +1,71 @@
+# Copyright 2019 Katsuya Shimabukuro. All rights reserved.
+# Licensed under the MIT License.
+from typing import Dict, List, Any
+import pathlib
+from dataset.base import DatasetBase
+from model.base import ModelBase
+
+
+class RunnerBase(object):
+    """Task runner base class."""
+
+    def __init__(self):
+        """Initilize parameters."""
+        self.datasets: dict
+        self.models: dict
+
+    def run(
+            self,
+            model_name: str,
+            dataset_name: str,
+            model_params: dict,
+            dataset_params: dict,
+            log_path: pathlib.Path) -> Dict[str, List[Any]]:
+        """Run task.
+
+        Args:
+            model_name (str): run model name.
+            dataset_name (str): run dataset name.
+            model_params (dict): model initialized parametaers.
+            dataset_params (dict): dataset initialized parametaers.
+            log_path (pathlib.Path): log path object.
+
+        Return:
+            history (Dict[str, List[Any]]): task running history.
+
+        """
+        # prepare dataset
+        if dataset_name in self.datasets:
+            dataset = self.datasets[dataset_name](**dataset_params)
+        else:
+            raise ValueError("dataset {} not found".format(dataset_name))
+
+        # prepare model
+        model_params['dataset'] = dataset
+        if model_name in self.models:
+            model = self.models[model_name](**model_params)
+        else:
+            raise ValueError("model {} not found".format(model_name))
+
+        history = self._run(dataset, model, log_path)
+        print("history", history)
+
+        return history
+
+    def _run(
+            self,
+            dataset: DatasetBase,
+            model: ModelBase,
+            log_path: pathlib.Path) -> Dict[str, List[Any]]:
+        """Run task.
+
+        Args:
+            dataset (DatasetBase): dataset object.
+            model (ModelBase): model object.
+            log_path (pathlib.Path): log path object.
+
+        Return:
+            history (Dict[str, List[Any]]): task running history.
+
+        """
+        raise NotImplementedError
---
+++ runner/image_recognition_trainer.py
@@ -0,0 +1,68 @@
+# Copyright 2019 Katsuya Shimabukuro. All rights reserved.
+# Licensed under the MIT License.
+from typing import Dict, List, Any
+import pathlib
+from matplotlib import pyplot as plt
+import seaborn
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from runner.base import RunnerBase
+from dataset.base import DatasetBase
+from dataset.mnist import MnistDataset
+from dataset.cifar10 import Cifar10Dataset
+from dataset.cifar100 import Cifar100Dataset
+from model.base import ModelBase
+from model.fcnn import FCNNClassifier
+from model.resnet import ResNet
+
+
+class ImageRecognitionTrainer(RunnerBase):
+    """Image recognition task trainning runner."""
+
+    def __init__(self):
+        """Initilize parameters."""
+        self.datasets = {
+            'mnist': MnistDataset,
+            'cifar10': Cifar10Dataset,
+            'cifar100': Cifar100Dataset,
+        }
+
+        self.models = {
+            'fcnn': FCNNClassifier,
+            'resnet': ResNet,
+        }
+
+    def _run(
+            self,
+            dataset: DatasetBase,
+            model: ModelBase,
+            log_path: pathlib.Path) -> Dict[str, List[Any]]:
+        """Run task.
+
+        Args:
+            dataset (DatasetBase): dataset object.
+            model (ModelBase): model object.
+            log_path (pathlib.Path): log path object.
+
+        Return:
+            history (Dict[str, List[Any]]): task running history.
+
+        """
+        # run learning
+        history = model.train()
+        model.save(log_path.joinpath('model.h5'))
+
+        # save results
+        y_pred, y_test = model.inference()
+        y_test = np.argmax(y_test, axis=1)
+        y_pred = np.argmax(y_pred, axis=1)
+
+        labels = list(range(10))
+        cm = confusion_matrix(y_test, y_pred, labels=labels)
+        df_cm = pd.DataFrame(cm, index=labels, columns=labels)
+        fig = plt.figure(figsize=(12.8, 7.2))
+        seaborn.heatmap(df_cm, cmap=plt.cm.Blues, annot=True)
+        fig.savefig(str(log_path.joinpath('confusion_matrix.png')))
+
+        return history
