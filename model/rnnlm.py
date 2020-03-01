812af3eb29d72aa6e7aa2ca5cc8555dfd03220f6
Katsuya SHIMABUKURO <katsu.generation.888@gmail.com>
2020-03-01 16:23:36 +0900

    Add to dropout and layer nums parameters

--- model/rnnlm.py
+++ model/rnnlm.py
@@ -13,6 +13,8 @@ class RNNLM(KerasLanguageModelBase):
             hidden_nums: int = 512,
             embedding_dim: int = 100,
             weight_decay: float = 1e-4,
+            dropout_rate: float = 0.5,
+            layer_nums: int = 2,
             **kwargs: Any) -> None:
         """Intialize parameter and build model."""
         super(RNNLM, self).__init__(**kwargs)
@@ -22,12 +24,15 @@ class RNNLM(KerasLanguageModelBase):
                                 embedding_dim,
                                 mask_zero=True)
 
-        self._rnn = tf.keras.layers.GRU(
-                            hidden_nums,
-                            return_sequences=True,
-                            stateful=False,
-                            recurrent_initializer='glorot_uniform',
-                            kernel_regularizer=tf.keras.regularizers.l2(weight_decay))
+        self._rnn: List[tf.keras.layers.Layer] = []
+        for _ in range(layer_nums):
+            self._rnn.append(tf.keras.layers.LSTM(
+                                hidden_nums,
+                                return_sequences=True,
+                                stateful=False,
+                                dropout=dropout_rate,
+                                recurrent_initializer='glorot_uniform',
+                                kernel_regularizer=tf.keras.regularizers.l2(weight_decay)))
 
         self._dense = tf.keras.layers.Dense(
                                         self.dataset.vocab_size,
@@ -39,7 +44,8 @@ class RNNLM(KerasLanguageModelBase):
         inputs = tf.keras.Input((self.dataset.seq_length, ))
         x = inputs
         x = self._embdeeing(x)
-        x = self._rnn(x)
+        for i in range(layer_nums):
+            x = self._rnn[i](x)
         x = self._dense(x)
 
         self.model = tf.keras.Model(inputs=inputs, outputs=x)
